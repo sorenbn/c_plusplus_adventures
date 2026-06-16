ac2c9081479bbb5cc38a31a7aba60b64923c40fb
sorenbn <sorenbn@outlook.com>
2026-06-16 19:03:10 +0200

    feat: basic variables

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -6,5 +6,36 @@ int main()
     std::cout << "Hello World" << '\n';
     std::cout << "How are you?" << '\n';
 
+    // int
+    int x;
+    x = 5;
+
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << sum << std::endl;
+
+    // double
+    double price = 13.99;
+    std::cout << price << std::endl;
+
+    // char
+    char grade = 'A';
+    char currency = '$';
+
+    std::cout << grade << std::endl;
+
+    // bool
+    bool is_student = true;
+    bool powered = false;
+
+    std::cout << is_student << std::endl;
+
+    // string
+    std::string name = "Dumpy";
+    std::string day_of_week = "Friday";
+
+    std::cout << name << ", is it " << day_of_week << " today?" << std::endl;
+
     return 0;
 }
