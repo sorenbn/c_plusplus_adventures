f7800ab151e8b74b8efa9d4c87c077e1e73bbda8
sorenbn <sorenbn@outlook.com>
2026-06-16 19:10:40 +0200

    feat: consts

---
+++ consts/consts.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm" << std::endl;
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    return 0;
+}
