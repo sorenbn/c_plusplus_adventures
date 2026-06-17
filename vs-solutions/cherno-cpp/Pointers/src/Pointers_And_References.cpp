40db7f430d926f79994f533209aeb5709ee011c1
sorenbn <sorenbn@outlook.com>
2026-06-17 21:52:23 +0200

    feat: references

---
+++ vs-solutions/cherno-cpp/Pointers/src/Pointers_And_References.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+// macros
+#define LOG(x) std::cout << x << std::endl
+
+// forward declerations
+void pointers();
+void references();
+void increment_by_pointer(int* number);
+void increment_by_reference(int& number);
+
+int main()
+{
+    pointers();
+    references();
+
+    std::cin.get();
+    return 0;
+}
+
+void pointers()
+{
+    LOG("######## POINTERS ########");
+
+    // variable
+    int number = 8;
+    LOG(number);
+
+    // pointer
+    int* number_pointer = &number;
+    // de-reference the pointer and set the value at that address
+    *number_pointer = 40;
+
+    LOG(number);
+
+    // address of number
+    increment_by_pointer(&number);
+    LOG(number);
+    // directly pass the number pointer we created
+    increment_by_pointer(number_pointer);
+    LOG(number);
+
+    // allocate a char* buffer on the heap (char = 1 byte, total = 8 bytes)
+    char* buffer = new char[8];
+    // initialize all memory to 0
+    memset(buffer, 0, 8);
+
+    // pointer to a pointer
+    char** buffer_pointer = &buffer;
+
+    // delete the memory again (using the delete[] array keyword)
+    delete[] buffer;
+}
+
+void references()
+{
+    LOG("######## REFERENCES ########");
+    // variable
+    int number = 5;
+    LOG(number);
+ 
+    // reference
+    int& number_reference = number;
+    number_reference = 2;
+    LOG(number);
+
+    increment_by_reference(number_reference);
+    LOG(number);
+
+    int number2 = 8;
+    // small gotcha, copies the value of number2 into the reference of number
+    // not possible to point to a new reference/value
+    number_reference = number2;
+    LOG(number);
+}
+
+void increment_by_pointer(int* number)
+{
+    // derefence first, and then increment
+    (*number)++;
+}
+
+void increment_by_reference(int& number) 
+{
+    number++;
+}
