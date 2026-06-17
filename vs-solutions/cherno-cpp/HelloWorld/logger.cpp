e31e14d9198e78e643507944ace4ce70800f169a
itm-sorenbn <soren.b.nielsen@it-minds.dk>
2026-06-17 14:32:32 +0200

    feat: cherno project

---
+++ vs-solutions/cherno-cpp/HelloWorld/logger.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <iostream>
+
+void log_message(std::string message) 
+{
+	std::cout << message << std::endl;
+}
---
+++ vs-solutions/cherno-cpp/HelloWorld/main.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+void log_message(std::string message);
+
+int main() 
+{
+	log_message("Hello, World");
+	return 0;
+}
