3f6bf68d96e8e6df4b134e8304b18b740ec20f50
sorenbn <sorenbn@outlook.com>
2026-06-17 15:40:21 +0200

    feat: debugger

--- vs-solutions/cherno-cpp/HelloWorld/main.cpp
+++ vs-solutions/cherno-cpp/HelloWorld/main.cpp
@@ -1,8 +1,19 @@
 #include "logger.h"
+#include <iostream>
 
 int main() 
 {
+	const char* message = "Hello, World";
+	const int length = 12;
+
 	init_logger();
-	log_message("Hello, World");
+	log_message(message);
+
+	for (int i = 0; i < length; i++)
+	{
+		const char c = message[i];
+		std::cout << c << std::endl;
+	}
+
 	return 0;
 }
