4043a8d6ce2041a794b72e59b726cfb89e9eda4d
sorenbn <sorenbn@outlook.com>
2026-06-24 00:00:09 +0200

    chore: better comments for static linking

--- vs-solutions/cherno-cpp/17_ExternalLibraries_StaticLinking/src/main.cpp
+++ vs-solutions/cherno-cpp/17_ExternalLibraries_StaticLinking/src/main.cpp
@@ -1,10 +1,3 @@
-#include <iostream>
-
-// if the headers are included in the project itself, use double quotes "lib.h", otherwise use angled brackets <lib.h>
-// angled brackets means it's external
-#include "GLFW/glfw3.h"
-//#include <GLFW/glfw3.h>
-
 /*
 	Static library linking:
 		The library gets 'embedded' into the executable itself (usually the way to go, if possible)
@@ -14,10 +7,24 @@
 	2 components required for setting up external library:
 		Include files, which is typically header files with all the symbols/variables & functions declarations
 		Library files, for the linker to know where to find all the symbol/variable & function definitions
+
+	This is done via the project properties for Visual Studio Projects (and is MSVC compiler-specific I assume?)
+		include files directory: Properties -> C/C++  -> General -> Additional Include Directories
+		library files directory: Properties -> Linker -> General -> Additional Library Directories
+		library files		   : Properties -> Linker -> Input   -> Additional Dependencies	
 */
+
+#include <iostream>
+
+// if the headers are included in the project itself, use double quotes "lib.h", otherwise use angled brackets <lib.h>
+// angled brackets means it's external
+#include "GLFW/glfw3.h"
+//#include <GLFW/glfw3.h>
 
 int main()
 {
 	int result = glfwInit();
 	std::cout << result << std::endl;
+
+	std::cin.get();
 }
