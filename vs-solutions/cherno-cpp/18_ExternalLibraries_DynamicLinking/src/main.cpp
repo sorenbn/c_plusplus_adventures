82f4aef0b764e0c68fe1053a4a3335f6e5aace75
sorenbn <sorenbn@outlook.com>
2026-06-24 00:25:25 +0200

    feat: dynamically linked library

--- vs-solutions/cherno-cpp/18_ExternalLibraries_DynamicLinking/src/main.cpp
+++ vs-solutions/cherno-cpp/18_ExternalLibraries_DynamicLinking/src/main.cpp
@@ -1,7 +1,7 @@
 /*
-	Static library linking:
-		The library gets 'embedded' into the executable itself (usually the way to go, if possible)
-
+	Dynamic library linking:
+		The library gets linked to your executable at runtime (requires the .dll file to physically be on the target machine running your executable, either next to it or somewhere on the machine itself)
+	
 	2 components required for setting up external library:
 		Include files, which is typically header files with all the symbols/variables & functions declarations
 		Library files, for the linker to know where to find all the symbol/variable & function definitions
@@ -9,15 +9,15 @@
 	This is done via the project properties for Visual Studio Projects (and is MSVC compiler-specific I assume?)
 		include files directory: Properties -> C/C++  -> General -> Additional Include Directories
 		library files directory: Properties -> Linker -> General -> Additional Library Directories
-		library files		   : Properties -> Linker -> Input   -> Additional Dependencies	
+		library files		   : Properties -> Linker -> Input   -> Additional Dependencies
+
+	Important thing to note about dynamically linked libraries:
+		The .dll must be next to the executable file, so copy/paste the glfw3.dll file into the build directory.
+		The input section for the linker must link to the 'glfw3dll.lib' file, instead of the 'glfw3.lib' file.
 */
 
 #include <iostream>
-
-// if the headers are included in the project itself, use double quotes "lib.h", otherwise use angled brackets <lib.h>
-// angled brackets means it's external
 #include "GLFW/glfw3.h"
-//#include <GLFW/glfw3.h>
 
 int main()
 {
